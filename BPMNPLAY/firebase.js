44e971fc194e1a637024391f08008c7ed720ccd8
belenquispi <belen.quispi@epn.edu.ec>
2018-05-08 09:35:52 -0500

    firebase

---
+++ BPMNPLAY/firebase.js
@@ -0,0 +1,94 @@
+
+var preguntas = [];
+var preguntaRandomica ;
+var resCorrecta;
+// Initialize Firebase
+
+var db = firebase.firestore();
+
+var settings = {
+    timestampsInSnapshots: true};
+
+db.settings(settings);
+
+
+function obtenerPreguntas()
+{
+    console.log("preguntassssss")
+   // var docRef = db.collection("preguntas");
+
+
+    db.collection("preguntas").get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+            preguntas.push(doc.data());
+            preguntas[preguntas.length-1].usada="falsa";
+        });
+
+    });
+}
+
+function guardarPregunta() {
+    var enunciado = document.getElementById('enunciado').value
+    var res1 = document.getElementById('res1').value
+    var res2 = document.getElementById('res2').value
+    var res3 = document.getElementById('res3').value
+    var res4 = document.getElementById('res4').value
+    var resCorrecta = document.getElementById('resCorrecta').value
+    var idMateria = "BPMN"
+
+    console.log(enunciado + res1 + res2 + res3 + res4 + resCorrecta + idMateria)
+
+    db.collection("preguntas").doc().set({
+        enunciado: enunciado,
+        res1: res1,
+        res2: res2,
+        res3: res3,
+        res4: res4,
+        resCorrecta: resCorrecta,
+        idMateria: idMateria
+    })
+        .then(function () {
+            alert("guardado")
+            console.log("Document successfully written!");
+        })
+        .catch(function (error) {
+            alert("No guardado")
+            console.error("Error writing document: ", error);
+        });
+}
+
+ function indiceRandomico()
+{
+    preguntaRandomica = Math.floor(Math.random()* preguntas.length)
+}
+
+
+function cargarPreguntas()
+{
+    indiceRandomico();
+    console.log("correcto")
+    console.log(preguntaRandomica);
+    if(preguntas[preguntaRandomica].usada == "falsa")
+    {
+        console.log("11111 " + preguntaRandomica);
+        document.getElementById("enunciado").innerHTML = preguntas[preguntaRandomica].enunciado;
+        document.getElementById("res1").innerHTML = preguntas[preguntaRandomica].res1;
+        document.getElementById("res2").innerHTML = preguntas[preguntaRandomica].res2;
+        document.getElementById("res3").innerHTML = preguntas[preguntaRandomica].res3;
+        document.getElementById("res4").innerHTML = preguntas[preguntaRandomica].res4;
+        resCorrecta = preguntas[preguntaRandomica].resCorrecta;
+
+        preguntas[preguntaRandomica].usada = "verdadera";
+        console.log(preguntas[preguntaRandomica].usada);
+    }
+    else
+    {
+        cargarPreguntas();
+    }
+
+
+
+}
+
+
