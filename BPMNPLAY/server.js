c461c3bd925807ef12188d1370c7aaba47a4ff5d
Santiago Lema <santiago.lema01@epn.edu.ec>
2018-05-18 12:08:32 -0500

    primera prueba
    A

---
+++ BPMNPLAY/server.js
@@ -0,0 +1,91 @@
+// Dependencies
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var socketIO = require('socket.io');
+var app = express();
+var server = http.Server(app);
+var io = socketIO(server);
+
+
+
+var gameMap = [
+    13, 14, 15, 16, 17, 18, 19, 20,
+    12, 0, 0, 0, 0, 0, 0, 21,
+    11, 10, 9, 0, 0, 24, 23, 22,
+    0, 0, 8, 0, 0, 25, 0, 0,
+    0, 0, 7, 0, 0, 26, 0, 0,
+    4, 5, 6, 0, 0, 27, 28, 29,
+    3, 0, 0, 0, 0, 0, 0, 30,
+    2, 1, "Inicio", 0, 0, 33, 32, 31
+];
+
+var colorMap = [];
+var anchoCasilla = 80, altoCasilla = 80;
+var columnas = 8, filas = 8;
+
+seleccionarColor();
+
+var parametrosJuego = {gameM: gameMap,
+    anchoCas : anchoCasilla,
+    altoCas : altoCasilla,
+    colum: columnas,
+    filas: filas,
+    colorM : colorMap}
+
+
+function seleccionarColor() {
+    var indice = 0;
+    for (var y = 0; y < filas; ++y) {
+        for (var x = 0; x < columnas; ++x) {
+            switch (gameMap[((y * columnas) + x)]) {
+                case 0:
+                    colorMap[indice] = -1;
+                    indice++;
+                    break;
+                default:
+                    var colorA = Math.floor(Math.random() * 3);
+                    switch (colorA) {
+                        case 0:
+                            colorMap[indice] = 0;
+                            indice++;
+                            break;
+                        case 1:
+                            colorMap[indice] = 1;
+                            indice++;
+                            break;
+                        case 2:
+                            colorMap[indice] = 2;
+                            indice++;
+                            break;
+                        default:
+                    }
+            }
+        }
+    }
+}
+
+
+
+app.set('port', 5000);
+app.use('/static', express.static(__dirname + '/static'));
+// Routing
+app.get('/', function(request, response) {
+    response.sendFile(path.join(__dirname, 'index.html'));
+});
+// Starts the server.
+server.listen(5000, function() {
+    console.log('Starting server on port 5000');
+});
+
+// Add the WebSocket handlers
+io.on('connection', function(socket) {
+    io.sockets.emit('parametrosJuego', parametrosJuego);
+});
+
+setInterval(function() {
+  //  io.sockets.emit('message', 'hi!');
+}, 1000)
+
+
+
---
+++ BPMNPLAY/static/game.js
@@ -0,0 +1,121 @@
+var ctx = null;
+var color1 = new Image();
+var color2 = new Image();
+var color3 = new Image();
+var filas, columnas, anchoCasilla, altoCasilla;
+var gameMap = [];
+var colorMap = [];
+
+var patterColor1, patterColor2, patterColor3;
+var currentSecond = 0, frameCount = 0, framesLastSecond = 0, lastFrameTime = 0;
+var gameTime = 0;
+var gameSpeeds = [
+    {name: "Normal", mult: 1},
+    {name: "Paused", mult: 0}
+];
+var currentSpeed = 0;
+
+var socket = io();
+socket.on('message', function(data) {
+    console.log(data);
+});
+
+
+window.onload = function () {
+    ctx = document.getElementById('game').getContext("2d");
+    requestAnimationFrame(drawGame);
+    ctx.font = "bold 10pt sans-serif";
+    console.log("adios")
+
+
+
+};
+
+socket.on('parametrosJuego', function(data) {
+    filas = data.filas;
+    columnas = data.colum;
+    anchoCasilla = data.anchoCas;
+    altoCasilla = data.altoCas;
+    gameMap = data.gameM;
+    colorMap = data.colorM;
+
+    console.log("hola")
+})
+
+function agregarNumerosCasilla() {
+    for (var y = 0; y < filas; ++y) {
+        for (var x = 0; x < columnas; ++x) {
+            switch (gameMap[((y * columnas) + x)]) {
+                case 0:
+                    //ctx.fillStyle = "#685b48";
+                    break;
+                default:
+                    ctx.fillStyle = "#000000";
+                    ctx.fillText(gameMap[((y * columnas) + x)], x * anchoCasilla + anchoCasilla / 2, y * altoCasilla + anchoCasilla / 2, anchoCasilla / 2, altoCasilla / 2);
+            }
+        }
+    }
+}
+
+function drawGame() {
+    if (ctx == null) {
+        return;
+    }
+    var currentFrameTime = Date.now();
+    var timeElapsed = currentFrameTime - lastFrameTime;
+    gameTime += Math.floor(timeElapsed * gameSpeeds[currentSpeed].mult);
+
+    var sec = Math.floor(Date.now() / 1000);
+    if (sec != currentSecond) {
+        currentSecond = sec;
+        framesLastSecond = frameCount;
+        frameCount = 1;
+    }
+    else {
+        frameCount++;
+    }
+
+
+    for (var y = 0; y < filas; ++y) {
+        for (var x = 0; x < columnas; ++x) {
+            color1.src = 'static/0.png';
+            color2.src = 'static/1.png';
+            color3.src = 'static/2.png';
+            patterColor1 = ctx.createPattern(color1, "repeat");
+            patterColor2 = ctx.createPattern(color2, "repeat");
+            patterColor3 = ctx.createPattern(color3, "repeat");
+
+            switch (gameMap[((y * columnas) + x)]) {
+                case 0:
+                    ctx.fillStyle = "#6A0888";
+                    break;
+                default:
+                    switch (colorMap[((y * columnas) + x)]) {
+                        case 0:
+                            ctx.fillStyle = patterColor1;
+                            break;
+                        case 1:
+                            ctx.fillStyle = patterColor2;
+                            break;
+                        case 2:
+                            ctx.fillStyle = patterColor3;
+                            break;
+                        default:
+                            ctx.fillStyle = "#5aa457";
+                    }
+            }
+            ctx.fillRect(x * anchoCasilla, y * altoCasilla, anchoCasilla, altoCasilla);
+        }
+    }
+
+    agregarNumerosCasilla();
+
+    ctx.fillStyle = "#ff0000";
+    ctx.fillText("FPS: " + framesLastSecond, 10, 20);
+
+    lastFrameTime = currentFrameTime;
+
+    requestAnimationFrame(drawGame);
+}
+
+
